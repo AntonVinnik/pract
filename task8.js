187b7e33a45b44d2800ca55cef01802dfdf42dff
AntonVinnik <145652092+AntonVinnik@users.noreply.github.com>
2023-12-21 17:20:48 +0200

    .

---
+++ task8.js
@@ -0,0 +1,9 @@
+const fruits = {'apple','plum','pear','orange','banana'};
+
+const firstTwoEls = fruits.slice(0,2);
+const nonExtremeEls = fruits.slice(1,-1);
+const lastTreeEls = fruits.slice(-3);
+
+console.log();
+console.log();
+console.log();
